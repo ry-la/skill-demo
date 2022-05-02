889f5c663b4e0cb46794163bac12c1da49da93d5
ry-la <ryla@ucsd.edu>
2022-05-01 22:52:04 -0700

    Added necessary files and code

---
+++ SkillDemoTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    
+    @Test
+    public void multiply() {
+        assertEquals(54, SkillDemo.multiply(7, 8));
+    }
+
+}
